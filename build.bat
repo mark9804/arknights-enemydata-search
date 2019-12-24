5ce72ed2ce16d7412bcfab5ea4835395771460ff
Mark Chen <fzmwczl@gmail.com>
2019-12-24 12:48:41 +0800

    这么久了才发现build.bat的路径全是我个人PC的路径

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-pyinstaller -F --clean --noupx --icon C:\Users\Mark\Desktop\各种小工具\arknights\Arknights_enemy_database\RHODES_ISLAND_ICON.ico C:\Users\Mark\Desktop\各种小工具\arknights\Arknights_enemy_database\Arknights_enemy_database.py --distpath C:\Users\Mark\Desktop\各种小工具\arknights\Arknights_enemy_database\
+pyinstaller -F --clean --noupx --icon %~dp0\RHODES_ISLAND_ICON.ico %~dp0\Arknights_enemy_database.py --distpath %~dp0\
